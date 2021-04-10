2c924521cab461c29b8f290ce9f48ff6fdb943c1
fabiola29298 <fabiola29298@gmail.com>
2021-04-10 14:47:35 -0400

    agregando boton de send whatsapp

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 
 import cipher from './cipher.js';
 
-console.log(cipher);
 
 let offSet = document.getElementById("offSet");
 let textArea = document.getElementById("textArea");
 let result = document.getElementById("result");
 
 const btnEncrypt = document.getElementById("encryptButton");
-// const btnDecrypt = document.getElementById("decryptButton");
+const btnDecrypt = document.getElementById("decryptButton");
 
 const btnCopy = document.getElementById("copyButton");
-// const btnSend = document.getElementById("whatsappButton");
+const btnSend = document.getElementById("whatsappButton");
 const btnExchange = document.getElementById("exchangeButton");
-
 
 
 btnEncrypt.addEventListener('click', () => {
@@ -21,18 +19,25 @@ btnEncrypt.addEventListener('click', () => {
     alert("Ingresa los datos")
   }
   else{
-    textArea.value = textArea.value.toUpperCase();
     // call function
-    // let newMessage =
-    let newMessage = textArea.value.toLowerCase();
+    let newMessage = cipher.encode(parseInt(offSet.value), textArea.value);
     // show new message
     result.value = newMessage;
-    console.log(offSet.value);
-    console.log(textArea.value);
 
   }
 });
+btnDecrypt.addEventListener('click', () => {
+  if (offSet.value === "" || textArea.value === "") {
+    alert("Ingresa los datos")
+  }
+  else {
+    // call function
+    let newMessage = cipher.decode(parseInt(offSet.value), textArea.value);
+    // show new message
+    result.value = newMessage;
 
+  }
+});
 btnExchange.addEventListener('click', () => {
   if (result.value === "") {
     alert("No se puede invertir, ingrese datos")
@@ -56,3 +61,22 @@ btnCopy.addEventListener('click', () => {
   tooltip.innerHTML = "Copied: " + result.value;
   // alert("Copied the text: " + result.value);
 });
+
+btnSend.addEventListener('click', () => {
+  console.log('gola');
+  var yourNumber = "558186580524";
+  var yourMessage = "testing";
+  let url = getLinkWhastapp(yourNumber, yourMessage);
+  window.open(url, '_blank').focus();
+  // var win = window.open(`https://wa.me/${num}?text=I%27m%20api%20msg%20hello%20${name}%20friend%20${msg}`, '_blank');
+});
+
+function getLinkWhastapp(number, message) {
+  var url =
+    "https://api.whatsapp.com/send?phone=" +
+    number +
+    "&text=" +
+    encodeURIComponent(message);
+
+  return url;
+}
