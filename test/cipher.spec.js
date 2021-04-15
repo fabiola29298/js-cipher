cb71d86b191ed4f9ad3eda918ffe5981987dfe1f
fabiola29298 <fabiola29298@gmail.com>
2021-04-15 16:14:44 -0400

    agregando tests para negativos y simbolos en combinacion

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -30,33 +30,34 @@ describe('cipher', () => {
       expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
 
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para letras minúsculas, descomente o teste
-    // abaixo.
-    //
+    it('should return "CHULZH HKYPHUH" for "VANESA ADRIANA" with offset 33', () => {
+      expect(cipher.encode(33, 'VANESA ADRIANA')).toBe('CHULZH HKYPHUH');
+    });
+
     it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
       expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     });
 
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
+    it('should return "chulzh hkyphuh" for "vanesa adriana" with offset 33', () => {
+      expect(cipher.encode(33, 'vanesa adriana')).toBe('chulzh hkyphuh');
+    });
+
     it('should return " !@" for " !@"', () => {
       expect(cipher.encode(33, ' !@')).toBe(' !@');
     });
+
+    it('should return "Ovsh! @Lsvu" for "Hola! @Elon"', () => {
+      expect(cipher.encode(33, 'Hola! @Elon')).toBe('Ovsh! @Lsvu');
+    });
+
+    it('should return "Gnkz @?Dknm" for "Hola! @Elon"', () => {
+      expect(cipher.encode(-1, 'Hola! @Elon')).toBe('Gnkz @?Dknm');
+    });
+
+    it('should return "Cjgv=<;Zgji" for "Hola! @Elon"', () => {
+      expect(cipher.encode(-5, 'Hola! @Elon')).toBe('Cjgv=<;Zgji');
+    });
+
   });
 
   describe('cipher.decode', () => {
@@ -76,33 +77,32 @@ describe('cipher', () => {
       expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
-    //
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para letras minúsculas, descomente o teste
-    // abaixo.
-    //
+    it('should return "VANESA ADRIANA" for "CHULZH HKYPHUH" with offset 33', () => {
+      expect(cipher.encode(33, 'CHULZH HKYPHUH')).toBe('VANESA ADRIANA');
+    });
+
     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
       expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
     });
 
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
-    // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
+    it('should return "vanesa adriana" for "chulzh hkyphuh" with offset 33', () => {
+      expect(cipher.encode(33, 'chulzh hkyphuh')).toBe('vanesa adriana');
+    });
+
     it('should return " !@" para " !@"', () => {
       expect(cipher.decode(33, ' !@')).toBe(' !@');
+    });
+
+    it('should return "Hola! @Elon" for "Ovsh! @Lsvu"', () => {
+      expect(cipher.encode(33, 'Ovsh! @Lsvu')).toBe('Hola! @Elon');
+    });
+
+    it('should return "Hola! @Elon" for "Gnkz @?Dknm"', () => {
+      expect(cipher.encode(-1, 'Gnkz @?Dknm')).toBe('Hola! @Elon');
+    });
+
+    it('should return "Hola! @Elon" for "Cjgv=<;Zgji"', () => {
+      expect(cipher.encode(-5, 'Cjgv=<;Zgji')).toBe('Hola! @Elon');
     });
   });
 
