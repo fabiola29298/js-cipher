6abb5c9910b7b13361e07e22ca825df86bf9b546
fabiola29298 <fabiola29298@gmail.com>
2021-04-15 16:40:17 -0400

    fix: arrglando cifrado y descifrado de simbolos

--- src/cipher.js
+++ src/cipher.js
@@ -4,24 +4,30 @@ const cipher = {
     if( typeof(text) != "string" || typeof(key)!= "number"){
       throw new TypeError("El texto no es válido");
     }
-    // catch offset negativo
-    if(key <0){
-      key += 26;
-    }
 
     let encryptToMessage = ""; // new message
     for (let i = 0; i < text.length; i++) {
       let charAscii = text.charCodeAt(i); // convert letters to unicodes
+      let newKey = key ;  // key change if charAscii is symbol or letter
+
+      // catch offset negativo
+      // if uppercase or lowercase
+      if (key < 0 && charAscii >= 65) { newKey = key + 26; }
+      // if symbols
+      if (key < 0 && charAscii < 65) { newKey = key + 33; }
+
+
       if (charAscii >= 65 && charAscii <= 90) {
         // uppercase letter range
-        encryptToMessage += String.fromCharCode(
-          ((charAscii - 65 + parseInt(key)) % 26) + 65);
-        // console.log(encryptToMessage);
+        encryptToMessage += String.fromCharCode(((charAscii - 65 + parseInt(newKey)) % 26) + 65);
       }
       else if (charAscii >= 97 && charAscii <= 122) {
         // lower case letter range
-        encryptToMessage += String.fromCharCode(
-          ((charAscii - 97 + parseInt(key)) % 26) + 97);
+        encryptToMessage += String.fromCharCode(((charAscii - 97 + parseInt(newKey)) % 26) + 97);
+      }
+      else if (charAscii >= 32 && charAscii <= 64) {
+        // symbols range
+        encryptToMessage += String.fromCharCode(((charAscii - 32 + parseInt(newKey)) % 33) + 32);
       } else {
         //different signs and characters are copied
         encryptToMessage += text.charAt(i);
@@ -34,22 +40,32 @@ const cipher = {
     if (typeof (text) != "string" || typeof (key) != "number") {
       throw new TypeError("El texto no es válido");
     }
-    // catch offset negativo
-    if (key < 0) {
-      key += 26;
-    }
-    let encryptToMessage = ""; // new message
 
+    let encryptToMessage = ""; // new message
     for (let i = 0; i < text.length; i++) {
-      let charAscii = text.charCodeAt(i);
-      if (charAscii >= 65 && charAscii <= 90 ) {
-        encryptToMessage += String.fromCharCode(
-          ((charAscii + 65 - parseInt(key)) % 26) + 65);
+      let charAscii = text.charCodeAt(i); // convert letters to unicodes
+      let newKey = key;  // key change if charAscii is symbol or letter
+
+      // catch offset negativo
+      // if uppercase or lowercase
+      if (key < 0 && charAscii >= 65) { newKey = key + 26; }
+      // if symbols
+      if (key < 0 && charAscii < 65) { newKey = key + 33; }
+
+
+      if (charAscii >= 65 && charAscii <= 90) {
+        // uppercase letter range
+        encryptToMessage += String.fromCharCode(((charAscii - 90 - parseInt(newKey)) % 26) + 90);
       }
       else if (charAscii >= 97 && charAscii <= 122) {
-        encryptToMessage += String.fromCharCode(
-          ((charAscii - 97 - parseInt(key) + 52) % 26) + 97);
+        // lower case letter range
+        encryptToMessage += String.fromCharCode(((charAscii - 122 - parseInt(newKey)) % 26) + 122);
+      }
+      else if (charAscii >= 32 && charAscii <= 64) {
+        // symbols range
+        encryptToMessage += String.fromCharCode(((charAscii - 64 - parseInt(newKey)) % 33) + 64);
       } else {
+        //different signs and characters are copied
         encryptToMessage += text.charAt(i);
       }
     }
